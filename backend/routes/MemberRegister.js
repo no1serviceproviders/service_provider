e7bad45ed9b71db0a8d7b4e8e030944941f2252e
no1serviceproviders <no1serviceproviders@gmail.com>
2026-04-05 23:56:44 +0530

    Update MemberRegister.js

--- backend/routes/MemberRegister.js
+++ backend/routes/MemberRegister.js
@@ -14,7 +14,7 @@ const { verifyToken } = require("../middleware/middleWare")
 
 router.post("/user/login", login)
 router.post("/user/register", register)
-router.post("/user/logout", logout)
+router.get("/user/logout", logout)
 router.get("/dashboard", verifyToken, dashboard)
 router.post('/payment/create-order',createOrder)
 router.post('/payment/verify',verifyPayment)
