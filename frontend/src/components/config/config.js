d926f93ab174b731e8b3bd8b152ae150f9dd9cac
no1serviceproviders <no1serviceproviders@gmail.com>
2026-04-06 00:36:04 +0530

    updated

--- frontend/src/components/config/config.js
+++ frontend/src/components/config/config.js
@@ -1,2 +1,2 @@
-export const base_url = "http://localhost:3000"
-export const url = "https://service-provider-frontend-hlu9.onrender.com"
+export const base_url = "https://service-provider-nbcy.onrender.com"
+export const url = ""
