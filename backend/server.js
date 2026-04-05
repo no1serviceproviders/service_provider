1c6bc81032dea5d993526cb2fbc6c2abac7a77d8
no1serviceproviders <no1serviceproviders@gmail.com>
2026-04-05 22:40:26 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,8 @@ const app = express()
 app.use(express.json())
 app.use(cors(
     {
-        origin: "http://localhost:5173",
+        // origin: "http://localhost:5173",
+        origin:"https://service-provider-nbcy.onrender.com"
         credentials:true
     }
 ))
