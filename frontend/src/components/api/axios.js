5bf63ad4db2007914c7d1802a977a66c6eceaf66
no1serviceproviders <no1serviceproviders@gmail.com>
2026-04-06 23:00:34 +0530

    Update axios.js

--- frontend/src/components/api/axios.js
+++ frontend/src/components/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://service-provider-nbcy.onrender.com",
   withCredentials: true
 })
 
