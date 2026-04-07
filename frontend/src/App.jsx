2f2def12f8451ceea2bd8ef429552ccf1ed130e2
no1serviceproviders <no1serviceproviders@gmail.com>
2026-04-07 23:34:09 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -43,7 +43,7 @@ const AnimatedRoutes = () => {
         />
 
   <Route 
-          path="/" 
+          path="/contact" 
           element={
             <ProtectedRoute>
               <ContactPage />
